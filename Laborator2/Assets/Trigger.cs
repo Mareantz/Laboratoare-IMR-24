8c304ac3a27d8093b052695f691d79c96bd11cc5
Cozloschi Luca <125896237+CaineleJoe@users.noreply.github.com>
2024-10-17 02:52:19 +0300

    gata
    gata

--- Laborator2/Assets/Trigger.cs
+++ Laborator2/Assets/Trigger.cs
@@ -1,65 +1,131 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
+using TMPro; 
 
 public class Trigger : MonoBehaviour
 {
-	public GameObject golfBatHandleBlack;
-	public GameObject golf;
-	public GameObject targetObject; // The GameObject you want to detect collisions with
+    public GameObject golfBatHandleBlack;
+    public GameObject golf;
+    public GameObject targetObject;
+    public ParticleSystem confettiParticles; 
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI scoreMessageText; 
 
-	private int score = 6;
-	private bool canDecreaseScore = true;
-	private float delay = 3.0f; // 3 second delay between score decreases
-	private bool hasEnteredTargetArea = false;
+    private int score = 6;
+    private bool canDecreaseScore = true;
+    private float delay = 3.0f; //score dissapear delay
+    private bool hasEnteredTargetArea = false;
+    private Vector3 initialGolfPosition;
+    void Start()
+    {
+        if (golfBatHandleBlack == null || golf == null || targetObject == null || confettiParticles == null || scoreText == null || scoreMessageText == null)
+        {
+            Debug.LogError("Please assign all necessary GameObjects in the inspector.");
+        }
+        initialGolfPosition = golf.transform.position;
 
-	void Start()
-	{
-		// Make sure all required objects are assigned
-		if (golfBatHandleBlack == null || golf == null || targetObject == null)
-		{
-			Debug.LogError("Please assign all necessary GameObjects in the inspector.");
-		}
-	}
+        //hide final score at the start
+        scoreMessageText.gameObject.SetActive(false);
+        UpdateScoreUI();
+    }
 
-	void Update()
-	{
-		// Continuously check if the golf bat is colliding with the golf ball
-		if (golfBatHandleBlack != null && golf != null && canDecreaseScore)
-		{
-			if (golfBatHandleBlack.GetComponent<Collider>().bounds.Intersects(golf.GetComponent<Collider>().bounds) && score > 1)
-			{
-				score--;
-				Debug.Log("Score decreased. Current Score: " + score);
-				StartCoroutine(ScoreDecreaseDelay());
-			}
-		}
-	}
+    void Update()
+    {
+        // Continuously check if the golf bat is colliding with the golf ball
+        if (golfBatHandleBlack != null && golf != null && canDecreaseScore)
+        {
+            if (golfBatHandleBlack.GetComponent<Collider>().bounds.Intersects(golf.GetComponent<Collider>().bounds) && score > 1)
+            {
+               
 
-	IEnumerator ScoreDecreaseDelay()
-	{
-		canDecreaseScore = false;
-		yield return new WaitForSeconds(delay);
-		canDecreaseScore = true;
-	}
+                StartCoroutine(ScoreDecreaseDelay());
+            }
+        }
+    }
 
-	void OnTriggerEnter(Collider other)
-	{
-		// Check if the golf ball hits the target object
-		if (other.gameObject == golf && other.bounds.Intersects(targetObject.GetComponent<Collider>().bounds) && !hasEnteredTargetArea)
-		{
-			hasEnteredTargetArea = true;
-			Debug.Log("Golf entered the target area! Current Score: " + score);
-			// You can add any additional actions here, such as triggering an event or updating UI
-		}
-	}
+    IEnumerator ScoreDecreaseDelay()
+    {
+        canDecreaseScore = false;
+        yield return new WaitForSeconds(delay);
+        canDecreaseScore = true;
+    }
 
-	void OnTriggerExit(Collider other)
-	{
-		// Reset the flag when the golf ball leaves the target area
-		if (other.gameObject == golf)
-		{
-			hasEnteredTargetArea = false;
-		}
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the golf ball hits the target object
+        if (other.gameObject == golf && other.bounds.Intersects(targetObject.GetComponent<Collider>().bounds) && !hasEnteredTargetArea)
+        {
+            hasEnteredTargetArea = true;
+            Debug.Log("Golf entered the target area! Current Score: " + score);
+
+            //Confetti animation
+            if (confettiParticles != null)
+            {
+                confettiParticles.Play();
+                Debug.Log("Confetti animation triggered!");
+            }
+
+            //Show scoring message
+            ShowScoreMessage();
+            ResetGolfBall();
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //Reset the flag when the golf ball leaves the target area
+        if (other.gameObject == golf)
+        {
+            hasEnteredTargetArea = false;
+        }
+    }
+    public void DecreaseScore()
+    {
+        if (canDecreaseScore && score > 1)
+        {
+            score--;
+            Debug.Log("Score decreased. Current Score: " + score);
+            UpdateScoreUI();
+        }
+    }
+        void UpdateScoreUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    
+    void ShowScoreMessage()
+    {
+        if (scoreMessageText != null)
+        {
+            scoreMessageText.text = "<align=center><color=#FF0000>YOU SCORED!!!!</color>\n<color=#FFFF00>Final Score: " + score + "</color></align>";
+            scoreMessageText.gameObject.SetActive(true);
+
+            StartCoroutine(HideScoreMessageAfterDelay(3.0f));
+        }
+    }
+
+    IEnumerator HideScoreMessageAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        scoreMessageText.gameObject.SetActive(false);
+    }
+
+
+    void ResetGolfBall()
+    {
+        golf.transform.position = initialGolfPosition;
+        Rigidbody golfRb = golf.GetComponent<Rigidbody>();
+        if (golfRb != null)
+        {
+            golfRb.velocity = Vector3.zero; 
+            golfRb.angularVelocity = Vector3.zero; 
+        }
+        score = 6;
+        UpdateScoreUI(); 
+    }
 }
---
+++ Laborator2/Assets/hithard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfHit : MonoBehaviour
+{
+    public GameObject golfBall;
+    public float hitForce = 200f;
+    public float verticalForce = 100f;
+
+    private Rigidbody golfBallRb;
+    private Trigger triggerScript;
+
+    void Start()
+    {
+        if (golfBall != null)
+        {
+            golfBallRb = golfBall.GetComponent<Rigidbody>();
+        }
+
+        triggerScript = FindObjectOfType<Trigger>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected with: " + collision.gameObject.name);
+
+        if (collision.gameObject == golfBall)
+        {
+            Vector3 hitDirection = (golfBall.transform.position - transform.position).normalized;
+
+            golfBallRb.AddForce(hitDirection * hitForce + Vector3.up * verticalForce);
+            Debug.Log("Golf ball hit with force! Direction: " + hitDirection);
+
+            if (triggerScript != null)
+            {
+                triggerScript.DecreaseScore();
+            }
+        }
+    }
+}
